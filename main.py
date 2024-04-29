97f623f9d9da02fe0aa046647c1f9256f2836186
Ryan Baldwin <ryanobaldwin@gmail.com>
2024-04-29 06:26:06 -0400

    RESOLVED WELCOME PAGE ERROR! :)

--- main.py
+++ main.py
@@ -13,6 +13,28 @@ from account_login  import user_login
 from menu import user_main, get_balance_for_user, deposit
 
 
-from starting_page import welcome_page
+def welcome_page():
+    while True:
 
+        print("Welcome to BANK OF BALDWINS!")
+        print("Options:")
+        print("1. Login")
+        print("2. Create Account")
+        print("3. Delete Account")
+        print("4. Exit Program")
+
+        choice = input("Enter your choice (1, 2, 3, or 4): ")
+
+        if choice == "1":
+            user_login()
+        elif choice == "2":
+            create_account()
+        elif choice == "3":
+            delete_account()
+        elif choice == "4":
+            print("Exiting the program. Thank you for using BANK OF BALDWIN!")
+            break
+        else:
+            print("Invalid choice. Please enter 1, 2, 3, or 4.")
+            
 welcome_page()
