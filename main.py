9048dc8bfbb773726599bda3ab874e032ff15f35
Ryan Baldwin <ryanobaldwin@gmail.com>
2024-04-29 06:40:39 -0400

    Attempting another time!

--- main.py
+++ main.py
@@ -37,31 +37,4 @@ def welcome_page():
         else:
             print("Invalid choice. Please enter 1, 2, 3, or 4.")
 
-def user_main(username):
-    while True:
-        print("\nOptions:")
-        print("1. Check Balance")
-        print("2. Deposit")
-        print("3. Withdraw")
-        print("4. Exit")
-        choice = input("Enter your choice (1, 2, 3, or 4): ")
-
-        if choice == "1":
-            get_balance_for_user(username)
-            print(f'Your current balance is ${get_balance_for_user(username):.2f}')
-        elif choice == "2":
-            deposit_amount_str = input(f'How much would you like to deposit today, {username}?')
-            deposit_amount = int(deposit_amount_str)
-            deposit(username, deposit_amount)
-        elif choice == "3":
-            withdraw_amount_str = input(f'How much would you like to withdraw today, {username}?')
-            withdraw_amount = int(withdraw_amount_str)
-            withdraw(username, withdraw_amount)
-        elif choice == "4":
-            print("Exiting the program. Thank you for using BANK OF BALDWIN!")
-            break
-        else:
-            print("Invalid choice. Please enter 1, 2, 3, or 4.")
-
 welcome_page()
-
