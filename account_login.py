29e1ae3e86543c6152f433d9386907fa16a4eeb9
Ryan Baldwin <ryanobaldwin@gmail.com>
2024-04-29 05:55:11 -0400

    Updated some input names.

--- account_login.py
+++ account_login.py
@@ -3,8 +3,9 @@ import mysql.connector
 # Global variable to store the logged-in username
 username = None
 
-def user_login(username_input, password_input):
+def user_login(user_name_input, password_input):
     global username
+    sign_in(user_name_input, password_input)
     connection = None
     try:
         # Connect to MySQL using the MySQL extension
@@ -18,13 +19,14 @@ def user_login(username_input, password_input):
         cursor = connection.cursor()
 
         # Check if the username and password match a row in the user_logins table
-        query = "SELECT user_name FROM c2c_banking_data.user_logins WHERE user_name = %s AND password = %s"
-        cursor.execute(query, (username_input, password_input))
+        query = "SELECT user_name & password FROM c2c_banking_data.user_logins WHERE user_name = %s AND password = %s"
+        cursor.execute(query, (user_name_input, password_input))
         result = cursor.fetchone()
 
         if result:
             # Login successful
-            username = username_input
+            username = user_name_input
+            password = password_input
             print("Login successful.")
         else:
             print("Incorrect username or password.")
@@ -44,7 +46,7 @@ def user_login(username_input, password_input):
         except Exception as ex:
             print("Error closing connection:", ex)
 
-def sign_in():
+def sign_in(user_name_input, password_input):
     user_name_input = input("Enter your username: ")
     password_input = input("Enter your password: ")
 
